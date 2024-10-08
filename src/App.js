2c63acd915c4d734c8122074938d56da6624ff35
CamPuter69 <t220023@famt.ac.in>
2024-10-08 11:34:31 +0530

    Initial commit 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RazorpayButton from './components/RazorpayButton';
 
 function App() {
   return (
@@ -18,6 +20,12 @@ function App() {
           Learn React
         </a>
       </header>
+
+      {/* Razorpay Payment Gateway Integration */}
+      <div>
+        <h1>Razorpay Payment Gateway Integration</h1>
+        <RazorpayButton />
+      </div>
     </div>
   );
 }
